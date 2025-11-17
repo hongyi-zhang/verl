b1d3a46bae65c0630e2934867d580eca3f6327b4
root <daryldongxf@gmail.com>
2025-11-17 11:14:07 +0000

    adding eval for intermediate steps

--- start_traning.sh
+++ start_traning.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python -m verl.trainer.main_ppo \
   data.val_files=/workspace/pcsd_data/news_all.parquet \
   data.max_prompt_length=6666 data.max_response_length=1024 \
   data.truncation=right data.filter_overlong_prompts=true \
-  data.train_batch_size=8 data.shuffle=false \
+  data.train_batch_size=8 data.shuffle=true \
   actor_rollout_ref.model.path=Qwen/Qwen2.5-0.5B-Instruct \
   actor_rollout_ref.actor.strategy=fsdp \
   actor_rollout_ref.actor.ppo_mini_batch_size=4 \
@@ -21,8 +21,8 @@ CUDA_VISIBLE_DEVICES=0 python -m verl.trainer.main_ppo \
   actor_rollout_ref.rollout.response_length=1024 \
   actor_rollout_ref.rollout.name=vllm \
   actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
-  actor_rollout_ref.rollout.temperature=0.7 \
-  actor_rollout_ref.rollout.top_p=0.9 \
+  actor_rollout_ref.rollout.temperature=0.9 \
+  actor_rollout_ref.rollout.top_p=0.95 \
   actor_rollout_ref.rollout.enable_chunked_prefill=false \
   actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
   +actor_rollout_ref.model.override_config.attn_implementation=flash_attention_2 \
